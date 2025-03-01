4586f8379a82b4a51e091f925efc861f2ca72480
Sarfo-Anokye <isaac.sarfo-anokye@amalitech.org>
2025-03-01 10:27:49 +0000

    chore: configured server

---
+++ server.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import express, {Application} from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const app: Application = express();
+
+// Middleware
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Routes
+// app.use("/api/users", userRoutes);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});

9438a9fe85ba73527b6a59f30512ad3b5b7ce552
Sarfo-Anokye <isaac.sarfo-anokye@amalitech.org>
2025-03-01 16:08:35 +0000

    chore: registered user dependencies

--- src/config/container.ts
+++ src/config/container.ts
@@ -1,6 +1,9 @@
 import {createContainer, asClass, asValue, InjectionMode} from "awilix";
 import prisma from "./prisma";
 import {LoggerService} from "../modules/logger/logger-service";
+import {PrismaUserRepository} from "../modules/user/prisma-user-repository";
+import {UserService} from "../modules/user/user-service";
+import {UserController} from "../modules/user/user-controller";
 
 const container = createContainer({
   injectionMode: InjectionMode.CLASSIC,
@@ -10,7 +13,10 @@ const container = createContainer({
 // Register dependencies
 container.register({
   prisma: asValue(prisma),
-  loggerService: asClass(LoggerService).singleton()
+  loggerService: asClass(LoggerService).singleton(),
+  userRepository: asClass(PrismaUserRepository).singleton(),
+  userService: asClass(UserService).singleton(),
+  userController: asClass(UserController).singleton()
 });
 
 export default container;

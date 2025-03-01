24b28719b71606c05f69f894275499fda8347790
Sarfo-Anokye <isaac.sarfo-anokye@amalitech.org>
2025-03-01 22:19:54 +0000

    feat: registerd movie dependencies to awilix container

--- src/config/container.ts
+++ src/config/container.ts
@@ -4,6 +4,9 @@ import {LoggerService} from "../modules/logger/logger-service";
 import {PrismaUserRepository} from "../modules/user/prisma-user-repository";
 import {UserService} from "../modules/user/user-service";
 import {UserController} from "../modules/user/user-controller";
+import {PrismaMovieRepository} from "../modules/movie/prisma-movie-repository";
+import {MovieController} from "../modules/movie/movie-controller";
+import {MovieService} from "../modules/movie/movie-service";
 
 const container = createContainer({
   injectionMode: InjectionMode.CLASSIC,
@@ -16,7 +19,10 @@ container.register({
   loggerService: asClass(LoggerService).singleton(),
   userRepository: asClass(PrismaUserRepository).singleton(),
   userService: asClass(UserService).singleton(),
-  userController: asClass(UserController).singleton()
+  userController: asClass(UserController).singleton(),
+  movieRepository: asClass(PrismaMovieRepository).singleton(),
+  movieService: asClass(MovieService).singleton(),
+  movieController: asClass(MovieController).singleton()
 });
 
 export default container;
